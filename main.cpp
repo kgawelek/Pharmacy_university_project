592e14190cee8d346ac9e1b1e240c042ffdf191f
Konrad Gawełek <56397059+kgawelek@users.noreply.github.com>
2020-10-27 11:20:07 +0100

    upload calego projektu

--- main.cpp
+++ main.cpp
@@ -8,36 +8,11 @@
 
 using namespace std;
 int main() {
-//    vector<string> c;
-//    c.push_back("alertec");
-//    Replacements * phead;
-//
-//
-//    Replacements r("allegra", nullptr);
-//    Prescription p(true, 100);
-//    Medicine medicine;
-//    medicine.name = "zyrtec";
-//    cout << medicine << endl;
-//    Pill pill;
-//    pill.name = "zyrtec";
+
 string data = "data.txt";
 MedicineList m;
 
 read_data(data, m);
-//auto p = m.pHead;
-//while(p){
-//    if(p->pill)
-//        cout << *(p->pill) << endl;
-//    else if (p->siroup)
-//        cout << *(p->siroup) << endl;
-//    else if(p->ointment)
-//        cout << *(p->ointment) << endl;
-//    else if (p->med)
-//        cout << *(p->med) << endl;
-//
-//    p = p->ptr_next;
-//}
-
 
 interface(m);
 
